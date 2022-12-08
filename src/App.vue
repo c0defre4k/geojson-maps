9584ea28ed46a4d034eb5a2b63973fcc260cc2c6
Ralph Pillichshammer <r.pillichshammer@neonblack.at>
2022-12-08 16:02:06 +0100

    ✨ add settings for GeoJson rendering

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,27 @@ import LeafletMap from '@/components/maps/LeafletMap.vue'
 import LeafletGeoJson from '@/components/maps/LeafletGeoJson.vue'
 
 const geoJson = ref(undefined)
+const geoJsonLabel = ref(undefined)
 const zoom = ref(7)
 const center = ref({ lat: 47.86, lng: 14.28 })
+const layerStyle = ref({
+  weight: 3,
+  color: '#ff3300',
+  opacity: 0.8,
+  fillColor: null,
+  fillOpacity: 0.18
+})
 </script>
 
 <template>
-  <aside class="bg-white basis-96 p-3 lg:p-4">
-    <RouterView />
+  <aside class="bg-white basis-96 p-3 lg:p-4 overflow-y-scroll">
+    <RouterView
+      v-model:zoom="zoom"
+      v-model:center="center"
+      v-model:layerStyle="layerStyle"
       @update:geoJson="geoJson = $event"
+      @update:geoJsonLabel="geoJsonLabel = $event"
+    />
   </aside>
 
   <main class="grow">
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,12 +1,251 @@
-<template>
-  <h1 class="text-2xl">Settings</h1>
-</template>
+<script setup>
+import { computed, ref, unref } from 'vue'
 
-<script>
-export default {
-  name: 'SettingsView'
+const props = defineProps({
+  zoom: {
+    type: Number,
+    default: 0
+  },
+  center: {
+    type: Object,
+    default: () => ({ lat: 0, lng: 0 })
+  },
+  layerStyle: {
+    type: Object,
+    default: Object
+  },
+  geoJsonLabel: {
+    type: String,
+    default: undefined
+  }
+})
+
+const emit = defineEmits([
+  'update:geoJson',
+  'update:geoJsonLabel',
+  'update:layerStyle',
+  'update:zoom',
+  'update:center'
+])
+
+const lat = computed({
+  get() {
+    return props.center.lat
+  },
+  set(value) {
+    emit('update:center', { lat: value, lng: unref(lng) })
+  }
+})
+
+const lng = computed({
+  get() {
+    return props.center.lng
+  },
+  set(value) {
+    emit('update:center', { lat: unref(lat), lng: value })
+  }
+})
+
+let geoJson = ref(null)
+const labels = computed(() => {
+  if (!geoJson.value) {
+    return []
+  }
+
+  const [{ properties }] = geoJson.value.features
+  return Object.keys(properties)
+})
+
+function updateStyle(prop, value) {
+  emit('update:layerStyle', {
+    ...props.layerStyle,
+    [prop]: value
+  })
+}
+
+function readGeoJson(e) {
+  const [file] = e.target.files
+  const reader = new FileReader()
+  reader.addEventListener('load', () => {
+    geoJson.value = JSON.parse(reader.result)
+    emit('update:geoJson', unref(geoJson))
+  })
+  reader.readAsText(file)
 }
 </script>
+
+<template>
+  <fieldset class="mb-4">
+    <h2 class="text-2xl mb-3">
+      GeoJSON
+    </h2>
+    <div class="mb-3">
+      <label for="geojson">Geojson</label>
+      <input
+        id="geojson"
+        type="file"
+        name="geojson"
+        accept="application/geo+json,application/json"
+        @change="readGeoJson"
+      >
+    </div>
+    <div class="mb-3">
+      <label for="geoJsonLabel">Label field</label>
+      <select
+        id="geoJsonLabel"
+        name="geoJsonLabel"
+        :value="geoJsonLabel"
+        @input="$emit('update:geoJsonLabel', $event.target.value)"
+      >
+        <option value="">
+          None
+        </option>
+        <option
+          v-for="label in labels"
+          :key="label"
+          :value="label"
+        >
+          {{ label }}
+        </option>
+      </select>
+    </div>
+
+    <h3 class="text-xl mb-3">
+      Layer style
+    </h3>
+    <div class="mb-3">
+      <label for="strokeWidth">Stroke width</label>
+      <input
+        id="strokeWidth"
+        :value="layerStyle.weight"
+        type="number"
+        name="strokeWidth"
+        step="1"
+        @input="updateStyle('weight', Number($event.target.value))"
+      >
+    </div>
+    <div class="mb-3">
+      <label for="strokeColor">Stroke color</label>
+      <div class="flex">
+        <input
+          id="strokeColor-hex"
+          :value="layerStyle.color"
+          type="text"
+          name="strokeColor-hex"
+          class="border-r-0 rounded-r-none"
+          pattern="#[0-9]{6}"
+          maxlength="7"
+          @input="updateStyle('color', $event.target.value)"
+        >
+        <input
+          id="strokeColor"
+          :value="layerStyle.color"
+          type="color"
+          name="strokeColor"
+          @input="updateStyle('color', $event.target.value)"
+        >
+      </div>
+    </div>
+    <div class="mb-3">
+      <label for="strokeOpacity">Stroke opacity</label>
+      <input
+        id="strokeOpacity"
+        :value="layerStyle.opacity"
+        type="range"
+        name="strokeOpacity"
+        step="0.01"
+        min="0"
+        max="1"
+        @input="updateStyle('opacity', Number($event.target.value))"
+      >
+      <small class="text-gray-700">{{ layerStyle.opacity }}</small>
+    </div>
+    <hr class="my-4">
+    <div class="mb-3">
+      <label for="fillColor">Fill color</label>
+      <div class="flex">
+        <input
+          id="fillColor-hex"
+          :value="layerStyle.fillColor || layerStyle.color"
+          type="text"
+          name="fillColor-hex"
+          class="border-r-0 rounded-r-none"
+          pattern="#[0-9]{6}"
+          maxlength="7"
+          @input="updateStyle('fillColor', $event.target.value)"
+        >
+        <input
+          id="fillColor"
+          :value="layerStyle.fillColor || layerStyle.color"
+          type="color"
+          name="fillColor"
+          @input="updateStyle('fillColor', $event.target.value)"
+        >
+      </div>
+    </div>
+    <div class="mb-3">
+      <label for="fillOpacity">Fill opacity</label>
+      <input
+        id="fillOpacity"
+        :value="layerStyle.fillOpacity"
+        type="range"
+        name="fillOpacity"
+        step="0.01"
+        min="0"
+        max="1"
+        @input="updateStyle('fillOpacity', Number($event.target.value))"
+      >
+      <small class="text-gray-700">{{ layerStyle.fillOpacity }}</small>
+    </div>
+  </fieldset>
+
+  <fieldset class="mb-4">
+    <h2 class="text-2xl mb-3">
+      Map
+    </h2>
+    <div class="mb-3">
+      <label for="zoom">Zoom</label>
+      <input
+        id="zoom"
+        :value="zoom"
+        type="number"
+        name="zoom"
+        step="1"
+        min="0"
+        max="20"
+        @input="$emit('update:zoom', Number($event.target.value))"
+      >
+    </div>
+
+    <h4 class="text-lg">
+      Center
+    </h4>
+    <div class="grid grid-cols-2 gap-2">
+      <div>
+        <label for="lat">Lat</label>
+        <input
+          id="lat"
+          v-model.number.lazy="lat"
+          type="number"
+          step="0.1"
+          name="lat"
+        >
+      </div>
+      <div>
+        <label for="lng">Lng</label>
+        <input
+          id="lng"
+          v-model.number.lazy="lng"
+          type="number"
+          step="0.1"
+          name="lng"
+        >
+      </div>
+    </div>
+  </fieldset>
+
+  <fieldset />
+</template>
 
 <style lang="scss">
 
